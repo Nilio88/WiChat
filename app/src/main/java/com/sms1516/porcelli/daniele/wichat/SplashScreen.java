296ed88ee6f00eb5475d86cbffb2d4f0be0f622c
Daniele Porcelli <d.porcelli@studenti.uniba.it>
2016-08-19 01:38:50 +0200

    Aggiunto il logo per lo splash screen e iniziato a scrivere codice per l'activity dello splash screen. Aggiunte le risorse di tipo dimensione per il logo.

--- app/src/main/java/com/sms1516/porcelli/daniele/wichat/SplashScreen.java
+++ app/src/main/java/com/sms1516/porcelli/daniele/wichat/SplashScreen.java
@@ -2,12 +2,42 @@ package com.sms1516.porcelli.daniele.wichat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import java.lang.Thread;
+import android.util.Log;
 
 public class SplashScreen extends AppCompatActivity {
+
+    //Costante per il tag di log
+    private static final String TAG_LOG = SplashScreen.class.getName();
+
+    //Costante per memorizzare il tempo di pausa dello splash screen
+    private static final long PAUSA = 3000L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //Rimuove i pulsanti di navigazione dalla schermata
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Mette in pausa lo splash screen
+        try {
+            Thread.sleep(PAUSA);
+        }
+        catch (InterruptedException ex) {
+            //In caso di interruzione, chiude l'applicazione
+            Log.e(TAG_LOG, ex.toString());
+            finish();
+        }
     }
 }
