a30d2502f01cf0e351adccee3c86ee5d6b47bce3
Daniele <Daniele>
2016-08-18 18:49:34 +0200

    Caricamento del progetto nella repository GitHub.

---
+++ app/src/main/java/com/sms1516/porcelli/daniele/wichat/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.sms1516.porcelli.daniele.wichat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
